2a968f6488fb91661582a665171226a5cf470331
michael_k <michael_kalamogan@emf.com.sg>
2022-09-05 12:36:44 +0800

    try catch

--- url-shortener-be/src/controllers/retrieveShortenedUrl.js
+++ url-shortener-be/src/controllers/retrieveShortenedUrl.js
@@ -1,13 +1,19 @@
 import express from "express";
 import prisma from "../../prisma/index.js";
+import log from "../logger/logs.js";
 
 const getController = {
   getUrl: async (req, res) => {
-    const { shortUrl } = req.params;
-    const record = await prisma.url.findUnique({ where: { shortUrl } });
-    if (record) {
-      res.redirect(record.originalUrl);
-    } else res.status(404).json({ data: "no such address" });
+    try {
+      const { shortUrl } = req.params;
+      const record = await prisma.url.findUnique({ where: { shortUrl } });
+      if (record) {
+        res.redirect(record.originalUrl);
+      } else res.status(404).json({ data: "no such address" });
+    } catch (error) {
+      log.error(error);
+      res.status(500).json(error);
+    }
   },
 };
 
